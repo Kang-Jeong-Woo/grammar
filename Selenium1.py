5a22a942da03f7636c6a115478ab8a94443b80c6
Kang-Jeong-Woo <zzangkbc1@gmail.com>
2022-05-17 13:41:43 +0900

    [update] Add basic grammar for web control
    about keyboard, mouse and ActionChains

--- Selenium1.py
+++ Selenium1.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common import action_chains
 
 
 #with webdriver.Chrome() as driver:
@@ -21,23 +23,51 @@ driver.get ('https://cafe.naver.com/kkangstylist?iframe_url=/ArticleList.nhn%3Fs
     # print(d.text)
     
 
-    #X.path 문법
+    ##X.path 문법##
     #/html/body/p
     #//p 모든 하위 노드를 반환
     #//div/p
     #/html/body//*
     #//p[@class='bold']
     
-    #x.path 활용
+    ##x.path 활용##
     #xpath=//p
     #a=driver.find_element_by_xpath(xpath)
     #xpath2 = "//*[contains(@class,'big')]"                 #클래스 중 big은 전부 가져 옴
 
-    #기다림.
+    ##기다림##
+    #1. time.sleep(10)
+    #2. driver.implicitly_wait(10)
+    #3. WebDriverWait
 e= WebDriverWait(driver, 10 ).until(
     EC.presence_of_all_elements_located((By.ID, 'id_name')))#명시적 기다리기. ID대신 XPATH나 CLASS, TAG 등 모두 가능.
     #element_to_be_clickable
-    
+
+    ##키보드와 마우스##
+driver.find_element_by_xpath('//*[@id="id"]').send_keys('zzangkbc1')
+driver.find_element_by_xpath('//*[@id="pw"]').send_keys('kangjeongwoo123'+Keys.ENTER)
+#ENTER, CONTROL, SHIFT, TAB, ESC, RETURN etc...
+driver.find_element_by_xpath('//*[@id="log.login"]').click()
+
+    ##Action Chains##
+webdriver.ActionChains(driver).action1().action2().perform()
+chains=webdriver.ActionChains(driver)
+chains.action1()
+chains.action2()
+chains.perform()
+id_= driver.find_element_by_id('id')
+pw_= driver.find_element_by_id('pw')
+webdriver.ActionChains(driver).send_keys_to_element(id_,'zzangkbc').send_keys_to_element(pw_,'zzangkbc123')\
+    .send_keys(Keys.ENTER).perform()
+bottom_=driver.find_element_by_xpath('//button')
+chains=webdriver.ActionChains(driver)   
+chains.key_down(Keys.CONTROL)                           #control을 누른채로
+chains.click(bottom_)                                   #클릭!
+chains.perform()
+chains.move_to_element(bottom_).pause(3).move_by_offset(100,100).perform()
+
+
+#아래는 실습 연습
 xpath="//[@class,'article-album-sub'']/img"
 img_list=driver.find_element_by_xpath(xpath)
 print(img_list)
